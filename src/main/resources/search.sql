725c6631261642c703147b1388b5f17234617190
githubzouzhiwei <wangyizouzhiwei@163.com>
2017-07-04 18:09:52 +0800

    更新SQL

--- src/main/resources/search.sql
+++ src/main/resources/search.sql
@@ -10,10 +10,26 @@ Target Server Type    : MYSQL
 Target Server Version : 50718
 File Encoding         : 65001
 
-Date: 2017-06-05 17:55:34
+Date: 2017-07-04 18:04:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户名',
+  `password` char(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '密码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('1', 'admin', 'e10adc3949ba59abbe56e057f20f883e');
 
 -- ----------------------------
 -- Table structure for t_article
@@ -22,14 +38,11 @@ DROP TABLE IF EXISTS `t_article`;
 CREATE TABLE `t_article` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标题',
-  `content` text COLLATE utf8mb4_unicode_ci COMMENT '内容',
+  `content` longtext COLLATE utf8mb4_unicode_ci,
+  `content_text` text COLLATE utf8mb4_unicode_ci COMMENT '内容文字',
+  `status` int(11) DEFAULT NULL COMMENT '状态 -1：删除 1：正常',
+  `create_at` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_at` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-ALTER TABLE t_article ADD COLUMN content_text text COLLATE utf8mb4_unicode_ci COMMENT '纯文本内容';
-ALTER TABLE t_article ADD COLUMN status int(11) DEFAULT NULL COMMENT '状态 -1：删除 1：正常';
-ALTER TABLE t_article ADD COLUMN create_at datetime DEFAULT NULL COMMENT '创建时间';
-ALTER TABLE t_article ADD COLUMN update_at datetime DEFAULT NULL COMMENT '更新时间';
-
--- 修改内容子段类型
-ALTER TABLE t_article MODIFY content longtext;
